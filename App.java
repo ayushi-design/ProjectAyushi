e5e8c4022e1e7c3782077958b6bc4a63ccf2a78c
Ayushi Patel <pate5752@sheridancollege.ca>
2026-06-01 17:46:04 -0400

    Added feature setup in App.java

--- App.java
+++ App.java
@@ -1,1 +1,1 @@
-"public class App { public static void main(String[] args) { System.out.println(\"Initial setup complete\"); } }" 
+public class App { public static void main(String[] args) { System.out.println("Initial setup complete"); System.out.println("Feature setup added"); } } 
